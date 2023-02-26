fb1e6426680a445952f8064f880b58371d94d30b
mwsierzega <marcelasierzega@MARCELAs-MacBook-Air.local>
2023-02-26 13:12:08 -0800

    final code, debugged

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,53 +1,84 @@
-# open csv files
+# open csv files, read csv file, write csv file
 
 import os
 import csv
 
-csvpath = os.path.join('Resources', 'election_data.csv')
-
-#read csv file
-
+csvpath = os.path.join('Resources', 'election_data.csv') 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+
+#skip header
+csv_header = next (csvreader)
+
+# Note to self, possible options to open csv files: 
+# csvpath = os.path.join(r"./Resources/election_data.csv",'rt')
+# csvpath = os.path.join("./Resources/election_data.csv")
+# with open("election_data.csv") as csvfile:
+    #csvreader = csv.reader(csvfile)
     
-    #skip the header
-    csv_header = next(csvreader)
+ 
+#variables
+total_ballots = 0
+fullcandidate_list = []
+candidate_list = []
+percent_Charles = 0
+total_Charles = 0
+counter_Charles = []
+percent_Diana = 0
+total_Diana = 0
+counter_Diana = []
+percent_Raymon = 0
+total_Raymon = 0
+counter_Raymon = []
     
-    #print analysis
-    print("Election Results")
-    print("-------------------------")
-    
-    #variables
-    totalballots = 0
-    fullcandidate_list = []
-    candidate_list = []
-    percent_Charlies = 0
-    total_Charlies = 0
-    counter_Charlies = []
-    
-    for row in csvreader:
-        totalballots = totalballots + 1
+for row in csvreader:
+        total_ballots = total_ballots + 1
         
-        #makes candidate column into a list
+        #candidate column into a list
         fullcandidate_list = [(row[2])]
         
-        #look through candidate colun list to get list of candidates
+        #look through candidate colum list
         for i in fullcandidate_list:
             if i not in candidate_list:
                 candidate_list.append(i)
-                
+        
+        #track vote counts for each candidate
         for i in fullcandidate_list:
             if i == candidate_list[0]:
-                counter_Charlies += [i]
-                
-        total_Charlies = len(counter_Charlies)
-        percent_Charlies = round((total_Charlies/totalballots)*100, 3)
+                counter_Charles += [i]
+            elif i == candidate_list[1]:
+                counter_Diana += [i]
+            elif i == candidate_list[2]:
+                counter_Raymon += [i]
         
-print(f"Total Votes: {totalballots}")
+        #initialize total vote counts with len function for each candidate    
+        total_Charles = len(counter_Charles)
+        percent_Charles = round((total_Charles/total_ballots)*100, 3)
+        
+        total_Diana = len(counter_Diana)
+        percent_Diana = round((total_Diana/total_ballots)*100, 3)
+        
+        total_Raymon = len(counter_Raymon)
+        percent_Raymon = round((total_Raymon/total_ballots)*100, 3)
+
+        # find winner of election
+        all_total_votes = total_Charles + total_Diana + total_Raymon
+        winner_value = max(counter_Charles, counter_Diana, counter_Raymon)
+
+        for i in range(len(all_total_votes)):
+            if all_total_votes[i] == winner_value:
+                winner = candidate_list[i]
+
+
+        
+#print analysis
+print("Election Results")
 print("-------------------------")
-print(f"{candidate_list[0]}: {percent_Charlies}% ({total_Charlies})")
-print(f"{candidate_list[1]}: ")
-print(f"{candidate_list[2]}: ")
+print(f"Total Votes: {total_ballots}")
 print("-------------------------")
-print(f"Winner: ")
+print(f"{candidate_list[0]}: {percent_Charles}% ({total_Charles})")
+print(f"{candidate_list[1]}: {percent_Diana}% ({total_Diana})")
+print(f"{candidate_list[2]}: {percent_Raymon}% ({total_Raymon})")
+print("-------------------------")
+print(f"Winner: {winner}")
 print("-------------------------")
